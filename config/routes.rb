7f7aa1d1f344400ef5d644b9114588b8300cd2aa
白石誠 <makotinator.15@gmail.com>
2020-06-20 22:28:49 +0900

    made the view of the header

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'statics/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "statics#index"
 end
